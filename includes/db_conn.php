8935fbacb9e308501382abc9e3ed63cc606b25ee
Harshit Prasad Singh <47072833+harshkyo@users.noreply.github.com>
2021-07-07 21:13:05 +0530

    e

--- includes/db_conn.php
+++ includes/db_conn.php
@@ -1,12 +1,21 @@
 <?php
 
-$sname= $_ENV["sname"];
+// $sname= $_ENV["sname"];
 
-$unmae= $_ENV["uname"];
+// $unmae= $_ENV["uname"];
 
-$password = $_ENV["password"];
+// $password = $_ENV["password"];
 
-$db_name = $_ENV["dbname"];
+// $db_name = $_ENV["dbname"];
+
+
+$sname= "us-cdbr-east-04.cleardb.com";
+
+$unmae= "b3535f9b8fd51c";
+
+$password = "ef83571e";
+
+$db_name = "heroku_1dceb4e469783fe";
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
